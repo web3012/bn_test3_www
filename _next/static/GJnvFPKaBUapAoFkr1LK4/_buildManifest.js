7213033a49f13b0e4f6aa7b74bd7a51258989975
web3012 <web3012@users.noreply.github.com>
2021-02-23 15:14:34 +0000

    deploy: web3012/bn_test3@754eb676f0eaecf01ebf8558f35eb343e5f42975

---
+++ _next/static/GJnvFPKaBUapAoFkr1LK4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ed26cd19fc34efdeff91.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-7c65c728d14e5f97d67d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
